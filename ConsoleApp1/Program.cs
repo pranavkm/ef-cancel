3154769b5481cbdde85a71c7988cda32e16d7a4d
Pranav K <prkrishn@hotmail.com>
2020-05-01 16:26:28 -0700

    Initial commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var ctx = new SampleContext();
+            var tcs = new TaskCompletionSource<bool>();
+            var cts = new CancellationTokenSource();
+
+            var t1 = Task.Run(async () =>
+            {
+                var task = ctx.LongTask(cts.Token);
+                tcs.TrySetResult(true);
+                await task;
+            });
+
+            var t2 = Task.Run(async () =>
+            {
+                await tcs.Task;
+                cts.Cancel();
+                await ctx.LongTask();
+            });
+
+            await Task.WhenAll(t1, t2);
+        }
+    }
+}
